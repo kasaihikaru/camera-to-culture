3db0534824f41504f31141e588ed99094dbd2ec7
笠井　光 <01018603@CA2606.local>
2018-01-08 11:04:39 +0900

    use_s3に戻した

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -5,21 +5,20 @@ class ImageUploader < CarrierWave::Uploader::Base
   # include CarrierWave::MiniMagick
 
   # Choose what kind of storage to use for this uploader:
-  # if use_s3?
-  #   storage :fog
-  # else
-  #   storage :file
-  # end
-
-  if Rails.env.development?
-    storage :file
-  # elsif Rails.env.test?
-  #   storage :file
-  else
+  if use_s3?
     storage :fog
+  else
+    storage :file
   end
 
-  # storage :fog
+  # if Rails.env.development?
+  #   storage :file
+  # elsif Rails.env.test?
+  #   storage :file
+  # else
+  #   storage :fog
+  # end
+
 
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,40 +1,41 @@
 # S3を使用しているかを判定するためのメソッド。
-# def use_s3?
-#   ENV['S3_ACCESS_KEY'] && ENV['S3_SECRET_KEY'] && ENV['S3_REGION'] && ENV['S3_BUCKET']
-# end
+def use_s3?
+  ENV['S3_ACCESS_KEY'] && ENV['S3_SECRET_KEY'] && ENV['S3_REGION'] && ENV['S3_BUCKET']
+end
 
 ## CarrierWaveの設定
 CarrierWave.configure do |config|
-  # S3の設定
-  config.fog_provider = 'fog/aws'
-  config.fog_credentials = {
-      :provider               => 'AWS',
-      :aws_access_key_id      => ENV['S3_ACCESS_KEY'],
-      :aws_secret_access_key  => ENV['S3_SECRET_KEY'],
-      :region                 => ENV['S3_REGION']
-      # :host                   => '必要なら設定する'
-      # :endpoint               => '必要なら設定する'
-  }
+  if use_s3?
+    # S3の設定
+    config.fog_provider = 'fog/aws'
+    config.fog_credentials = {
+        :provider               => 'AWS',
+        :aws_access_key_id      => ENV['S3_ACCESS_KEY'],
+        :aws_secret_access_key  => ENV['S3_SECRET_KEY'],
+        :region                 => ENV['S3_REGION']
+        # :host                   => '必要なら設定する'
+        # :endpoint               => '必要なら設定する'
+    }
 
-  # S3のバケットを指定。
-  case Rails.env
-    when 'production'
-        config.fog_directory  = ENV['S3_BUCKET']
-        config.asset_host = 'https://camera-to-culture-ver2.s3-ap-northeast-1.amazonaws.com'
-    when 'staging'
-        config.fog_directory  = ENV['S3_BUCKET']
-    when 'development'
-        config.fog_directory  = ENV['S3_BUCKET']
-    when 'test'
-        config.fog_directory  = ENV['S3_BUCKET']
+    # S3のバケットを指定。
+    case Rails.env
+      when 'production'
+          config.fog_directory  = ENV['S3_BUCKET']
+          # config.asset_host = 'https://camera-to-culture-ver2.s3-ap-northeast-1.amazonaws.com'
+      when 'staging'
+          config.fog_directory  = ENV['S3_BUCKET']
+      when 'development'
+          config.fog_directory  = ENV['S3_BUCKET']
+      when 'test'
+          config.fog_directory  = ENV['S3_BUCKET']
+    end
+
+    # 一般公開させて無いS3の場合は以下の設定を行う。
+    config.fog_public = false
+    # 一般公開されていない場合は以下の設定をする事で60秒間有効なURLを発行してくれる。
+    config.fog_authenticated_url_expiration = 60
+    CarrierWave::SanitizedFile.sanitize_regexp = /[^[:word:]\.\-\+]/
   end
-
-  # 一般公開させて無いS3の場合は以下の設定を行う。
-  config.fog_public = true
-  # 一般公開されていない場合は以下の設定をする事で60秒間有効なURLを発行してくれる。
-  config.fog_authenticated_url_expiration = 60
-  CarrierWave::SanitizedFile.sanitize_regexp = /[^[:word:]\.\-\+]/
-
 
   # public配下にキャッシュができると参照されてしまうので、予め変えておく。
   config.cache_dir = "#{Rails.root}/tmp/uploads"
