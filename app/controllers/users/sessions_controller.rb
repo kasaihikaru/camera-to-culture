d386956884c1dc1675f0a7035ba584706d00b88c
笠井　光 <01018603@CA2606.local>
2018-03-17 15:30:47 +0900

    ログイン処理など直し

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -7,19 +7,33 @@ class Users::SessionsController < Devise::SessionsController
   # end
 
   # POST /resource/sign_in
-  # def create
-  #   super
-  # end
+  def create
+    # super
+
+    self.resource = warden.authenticate!(auth_options)
+    set_flash_message!(:notice, :signed_in)
+    sign_in(resource_name, resource)
+    yield resource if block_given?
+
+    if search_result_params[:redirect_client_id].present?
+      redirect_to new_user_message_path(current_user.id, search_result_params)
+    else
+      respond_with resource, location: after_sign_in_path_for(resource)
+    end
+  end
 
   # DELETE /resource/sign_out
   # def destroy
   #   super
   # end
 
-  # protected
+  protected
 
   # If you have extra params to permit, append them to the sanitizer.
   # def configure_sign_in_params
   #   devise_parameter_sanitizer.permit(:sign_in, keys: [:attribute])
   # end
+  def search_result_params
+    params.require(:user).permit(:redirect_client_id)
+  end
 end
