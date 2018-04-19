5f57d4a918329da21ce65df09118b5e1fe1ac960
笠井　光 <01018603@CA2606.local>
2018-04-20 02:13:53 +0900

    prim-color変更

--- app/controllers/event_reviews_controller.rb
+++ app/controllers/event_reviews_controller.rb
@@ -1,10 +1,23 @@
 class EventReviewsController < ApplicationController
 	def create
-		
+		EventReview.create(create_params)
 	end
 
 	def update
-		
+		ev_rev = EventReview.find(id_params)
+		ev_rev.update(create_params)
 	end
 
+private
+	def create_params
+		 params.require(:event_review).permit(:cl_review).merge(event_id: params[:event_id])
+	end
+
+	def update_params
+		 params.require(:event_review).permit(:cl_review).merge(event_id: params[:event_id])
+	end
+
+	def id_params
+		params[:id]
+	end
 end
