6c4da07793bcac78db5f45a985057e20f0599088
笠井　光 <01018603@CA2606.local>
2018-05-04 17:39:13 +0900

    微修正

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -52,7 +52,7 @@ Rails.application.configure do
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
 
-# メール設定
+  # メール設定
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
